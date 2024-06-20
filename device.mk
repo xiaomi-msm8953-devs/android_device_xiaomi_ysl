80d1757824bcfcf71584229bc93a1b666e8dca9b
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-06-19 21:20:03 -0400

    try to fix qti error

--- device.mk
+++ device.mk
@@ -48,3 +48,9 @@ PRODUCT_PACKAGES += \
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/ysl/ysl-vendor.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
+
